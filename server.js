2e3c5a1f82bb85f23ae9d30a50071d058414d723
den-prof <93768261+den-prof@users.noreply.github.com>
2023-01-02 07:36:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -86,9 +86,8 @@ app.get("*", function (res, req) {
 
 const MONGO_DB_URL = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.pn5jxqp.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 
-mongoose.connect(MONGO_DB_URL)
-    .then(() => {
-        app.listen(process.env.PORT || port)
-    })
+app.listen(process.env.PORT || port)
+
+mongoose.connect(MONGO_DB_URL);
 
 
